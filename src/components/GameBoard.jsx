2edd393e7f2fa18c6326c90e07e41acca5a98035
Rafay Mirza <mirzaaravi@gmail.com>
2025-12-30 00:15:10 -0500

    Added animation

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,17 +1,23 @@
+import { motion } from "framer-motion";
 
-export default function GameBoard({cards, onCardsClick}){
-   return (<>
-   <div className="game-board">
-    {cards.map(cards => (
-        <img
-
-        key={cards.id}
-        src={cards.image}
-        alt={cards.name}
-        onClick={() => onCardsClick(cards.id)}
-
+export default function GameBoard({ cards, onCardsClick }) {
+  return (
+    <div className="game-board">
+      {cards.map((card) => (
+        <motion.img
+          key={card.id}
+          src={card.image}
+          alt="character"
+          className="card"
+          onClick={() => onCardsClick(card.id)}
+          layout
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={{ opacity: 1, scale: 1 }}
+          whileHover={{ scale: 1.12 }}
+          whileTap={{ scale: 0.95 }}
+          transition={{ type: "spring", stiffness: 300, damping: 20 }}
         />
-    ))}
-   </div>
-   </>)
+      ))}
+    </div>
+  );
 }
