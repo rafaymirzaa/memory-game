53d4e84bda2c738d2c6d716de97e43b1034fde75
Rafay Mirza <mirzaaravi@gmail.com>
2025-12-29 23:27:18 -0500

    Minor Logic fixes

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,9 @@ export default function App (){
         const res = await fetch ('https://rickandmortyapi.com/api/character')
         const data = await res.json()
        
-        const imageTile = data.results.slice(0, 6).map((char) => ({
+        const imageTile = data.results
+        .slice(0, 9).filter((f,index)=>index !== 5)
+        .map((char) => ({
         id: char.id,
         image: char.image
       }))
@@ -23,10 +25,19 @@ export default function App (){
     fetchCard()
    },[])
 
-    
+    function shuffleCards(arr){
+        let shuffled = [... arr]
+
+        for (let i = shuffled.length() - 1 ; i > 0; i--){
+           let j = Math.floor(Math.random() * i)
+           [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]
+        }
+    }
 
    return (<> 
    <GameBoard
+    cards = {image}
+    onCardsClick={shuffleCards}
    />
    </>)
     
